cc7c71584cec6fb445998e40ead52105d597d928
Danai <danai.th.64@ubu.ac.th>
2024-11-05 20:13:36 +0700

    DB to GitHub

--- envubuntu/bin/activate.csh
+++ envubuntu/bin/activate.csh
@@ -1,5 +1,6 @@
 # This file must be used with "source bin/activate.csh" *from csh*.
 # You cannot run it directly.
+
 # Created by Davide Di Blasi <davidedb@gmail.com>.
 # Ported to Python 3.3 venv by Andrew Svetlov <andrew.svetlov@gmail.com>
 
@@ -8,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/danai/Desktop/dssi-project_สมบูรณ์ - Copy (2)/envubuntu"
+setenv VIRTUAL_ENV "/home/danai/Desktop/projecrt-2-67/envubuntu"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
--- envubuntu/bin/activate.fish
+++ envubuntu/bin/activate.fish
@@ -1,5 +1,5 @@
 # This file must be used with "source <venv>/bin/activate.fish" *from fish*
-# (https://fishshell.com/); you cannot run it directly.
+# (https://fishshell.com/). You cannot run it directly.
 
 function deactivate  -d "Exit virtual environment and return to normal shell environment"
     # reset old environment variables
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/danai/Desktop/dssi-project_สมบูรณ์ - Copy (2)/envubuntu"
+set -gx VIRTUAL_ENV "/home/danai/Desktop/projecrt-2-67/envubuntu"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
